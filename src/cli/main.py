2ce857085de65c6d252cf9d179d8d4e93099fe84
ritvikr-codes <ritvikraam@gmail.com>
2026-08-08 17:31:26 +0530

    Add brute force attack CLI command

--- src/cli/main.py
+++ src/cli/main.py
@@ -17,6 +17,11 @@ from ..attacks.dictionary_attack import (
     crack_bcrypt,
 )
 
+from src.attacks.brute_force import (
+    crack_sha256 as brute_force_sha256,
+    crack_sha512 as brute_force_sha512,
+)
+
 from ..utils.wordlist_loader import load_wordlist
 
 def hash_command(args):
@@ -49,6 +54,31 @@ def dictionary_command(args):
             wordlist,
         )
 
+    if result is None:
+        print("Password not found.")
+    else:
+        print("SUCCESS!")
+        print(f"Recovered password : {result}")
+
+def bruteforce_command(args):
+    # Handle the brute-force attack command.
+    print("\nBrute Force Attack\n")
+    print(f"Algorithm   : {args.algorithm}")
+    print(f"Charset     : {args.charset}")
+    print(f"Max length  : {args.max_length}")
+    print("\nTrying password candidates...\n")
+    if args.algorithm == "sha256":
+        result = brute_force_sha256(
+            args.hash,
+            args.charset,
+            args.max_length,
+        )
+    else:
+        result = brute_force_sha512(
+            args.hash,
+            args.charset,
+            args.max_length,
+        )
     if result is None:
         print("Password not found.")
     else:
@@ -109,6 +139,39 @@ def main():
     dictionary_parser.set_defaults(
         func=dictionary_command
     )
+
+    # bruteforce command
+    bruteforce_parser = subparsers.add_parser(
+        "bruteforce",
+        help="Run a brute-force attack",
+    )
+
+    bruteforce_parser.add_argument(
+        "--algorithm",
+        required=True,
+        choices=["sha256", "sha512"],
+    )
+
+    bruteforce_parser.add_argument(
+        "--hash",
+        required=True,
+    )
+
+    bruteforce_parser.add_argument(
+        "--charset",
+        required=True,
+    )
+
+    bruteforce_parser.add_argument(
+        "--max-length",
+        type=int,
+        required=True,
+    )
+
+    bruteforce_parser.set_defaults(
+        func=bruteforce_command
+    )
+
     args = parser.parse_args()
     args.func(args)
 
